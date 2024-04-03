f57016b28fefa01cb7b6e94ce579ed476314c6eb
prianka dey <125360067+deyprianka25@users.noreply.github.com>
2024-04-03 23:07:52 +0530

    Time: 0 ms (100.00%), Space: 43.6 MB (98.60%) - LeetHub

---
+++ 2057-smallest-index-with-equal-value/2057-smallest-index-with-equal-value.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int smallestEqual(int[] nums) {
+        int index = -1;
+        for(int i=0;i<nums.length;i++){
+            if(i % 10==nums[i]){
+                index = i;
+                break;
+            }
+        }
+        return index;
+        
+    }
+}
