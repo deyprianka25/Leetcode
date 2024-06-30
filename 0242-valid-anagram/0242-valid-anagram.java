4fb7e598abaa2ed446f01a984ba04ef381f53764
prianka dey <125360067+deyprianka25@users.noreply.github.com>
2024-06-30 16:52:31 +0530

    Time: 18 ms (17.19%), Space: 44.6 MB (20.66%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,24 +1,29 @@
 class Solution {
-    public boolean isAnagram(String s, String t) {
-        HashMap<Character,Integer> map = new HashMap<>();
+       public HashMap createHM(String s){
+        HashMap<Character,Integer> hm=new HashMap<>();
         for(int i=0;i<s.length();i++){
-            char ch = s.charAt(i);
-            map.put(ch, map.getOrDefault(ch,0)+1);
-            
-        }
-        for(int i=0;i<t.length();i++){
-            char ch = t.charAt(i);
-            if(map.containsKey(ch)==false){
-                return false;
-                
-            }else if(map.get(ch)==1){
-                map.remove(ch);
-            }else{
-                map.put(ch,map.get(ch)-1);
+            if(hm.get(s.charAt(i))==null){
+                hm.put(s.charAt(i),1);
             }
-            
+            else{
+                hm.put(s.charAt(i),hm.get(s.charAt(i))+1);
+            }
         }
-        return map.size()==0;
-        
+        return hm;
+    }
+     public boolean isAnagram(String s, String t) {
+        if(s.length()==0 && t.length()==0){
+            return true;
+        }
+        HashMap<Character,Integer> hm1=new HashMap<>();
+        HashMap<Character,Integer> hm2=new HashMap<>();
+        hm1=createHM(s);
+        hm2=createHM(t);
+        if(hm1.equals(hm2)){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }
