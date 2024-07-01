9626c53b31851d8416c8d88e91c84dbad392bd2c
prianka dey <125360067+deyprianka25@users.noreply.github.com>
2024-07-01 14:06:15 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (27.82%) - LeetHub

---
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        for (int i = 0; i < arr.length - 2; i++) {
+            // Check if the current element and the next two elements are all odd
+            if (arr[i] % 2 == 1 && arr[i + 1] % 2 == 1 && arr[i + 2] % 2 == 1) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
