776874601fc4194e58650e04e8a98417560f53fb
prianka dey <125360067+deyprianka25@users.noreply.github.com>
2024-07-12 11:09:48 +0530

    Time: 3 ms (26.16%), Space: 43.1 MB (56.42%) - LeetHub

---
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findGCD(int[] nums) {
+        int min = Arrays.stream(nums).min().getAsInt();
+        int max = Arrays.stream(nums).max().getAsInt();
+
+        // Compute the GCD of min and max
+        return gcd(min, max);
+    }
+
+    // Helper method to compute GCD using the Euclidean algorithm
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+        
+    }
