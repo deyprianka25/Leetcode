113d4ea05be591f8ebe3312760082903ff35d9f7
prianka dey <125360067+deyprianka25@users.noreply.github.com>
2024-08-05 13:24:37 +0530

    Time: 4 ms (70.75%), Space: 44.8 MB (16.48%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,29 +1,19 @@
 class Solution {
-       public HashMap createHM(String s){
-        HashMap<Character,Integer> hm=new HashMap<>();
-        for(int i=0;i<s.length();i++){
-            if(hm.get(s.charAt(i))==null){
-                hm.put(s.charAt(i),1);
-            }
-            else{
-                hm.put(s.charAt(i),hm.get(s.charAt(i))+1);
-            }
-        }
-        return hm;
-    }
-     public boolean isAnagram(String s, String t) {
-        if(s.length()==0 && t.length()==0){
-            return true;
-        }
-        HashMap<Character,Integer> hm1=new HashMap<>();
-        HashMap<Character,Integer> hm2=new HashMap<>();
-        hm1=createHM(s);
-        hm2=createHM(t);
-        if(hm1.equals(hm2)){
-            return true;
-        }
-        else{
+      public static boolean isAnagram(String s, String t) {
+           if (s.length() != t.length()) {
             return false;
         }
+
+        // Convert both strings to character arrays
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
+
+        // Sort both character arrays
+        Arrays.sort(sArray);
+        Arrays.sort(tArray);
+
+        // Compare the sorted arrays
+        return Arrays.equals(sArray, tArray);
     }
 }
+        
