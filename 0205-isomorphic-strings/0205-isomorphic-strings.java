9b363f7dc065b57d7b3a51603afa899fdf7a3cd0
prianka dey <125360067+deyprianka25@users.noreply.github.com>
2024-08-07 00:13:38 +0530

    Time: 5 ms (96.59%), Space: 42.4 MB (48.94%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,14 +1,32 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-         Map<Character, Integer> charToIndex_s = new HashMap<>();
-    Map<Character, Integer> charToIndex_t = new HashMap<>();
+        // Map<Character, Integer> charToIndex_s = new HashMap<>();
+   // Map<Character, Integer> charToIndex_t = new HashMap<>();
 
-    for (Integer i = 0; i < s.length(); ++i)
-      if (charToIndex_s.put(s.charAt(i), i) != charToIndex_t.put(t.charAt(i), i))
-        return false;
+   // for (Integer i = 0; i < s.length(); ++i)
+     // if (charToIndex_s.put(s.charAt(i), i) != charToIndex_t.put(t.charAt(i), i))
+       // return false;
 
-    return true;
-  }
+   // return true;
+ // }
+//}
+
+
+if (s.length() != t.length()) return false;
+        int[] mapS = new int[256];
+        int[] mapT = new int[256];
+        for (int i = 0; i < s.length(); i++) {
+            char charS = s.charAt(i);
+            char charT = t.charAt(i);
+            if (mapS[charS] != mapT[charT]) {
+                return false;
+            }
+            mapS[charS] = i + 1;
+            mapT[charT] = i + 1;
+        }
+        
+        return true;
+    }
 }
             
         
