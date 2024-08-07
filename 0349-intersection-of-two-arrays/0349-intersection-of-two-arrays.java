0114d37b66e709fb7513593f4ece55baaa12f12c
prianka dey <125360067+deyprianka25@users.noreply.github.com>
2024-08-07 08:08:44 +0530

    Time: 2 ms (95.42%), Space: 43.3 MB (30.75%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,21 +1,47 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-       ArrayList<Integer> result = new ArrayList<>();
-        for(int i=0;i<nums1.length;i++){
-            for(int j=0;j<nums2.length;j++){
-                if(nums1[i]==nums2[j]){
-                    if(!result.contains(nums1[i])){
-                        result.add(nums1[i]);
-                    }
-                    break;
-                }
+        //SET APPROACH
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+        for(int num:nums1){
+            set1.add(num);
+        }
+        for(int num: nums2){
+            if(set1.contains(num)){
+                set2.add(num);
             }
+            
         }
-        int[] intersection = new int[result.size()];
-        for(int i=0;i<result.size();i++){
-            intersection[i]=result.get(i);
+         int[] result = new int[set2.size()];
+        int i = 0;
+        for (int num : set2) {
+            result[i++] = num;
         }
-        return intersection;
+
+        return result;
     }
 }
+
+        
+        
+        
+        //BRUTEFORCR APPROACH
+      // ArrayList<Integer> result = new ArrayList<>();
+        //for(int i=0;i<nums1.length;i++){
+            //for(int j=0;j<nums2.length;j++){
+                //if(nums1[i]==nums2[j]){
+                    //if(!result.contains(nums1[i])){
+                        //result.add(nums1[i]);
+                    //}
+                    //break;
+                //}
+            //}
+       // }
+       // int[] intersection = new int[result.size()];
+       // for(int i=0;i<result.size();i++){
+            //intersection[i]=result.get(i);
+        //}
+       // return intersection;
+   // }
+//}
         
