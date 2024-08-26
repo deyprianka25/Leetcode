9b066e95a567c463f9ec2d948a2e5f79f4a7a76a
prianka dey <125360067+deyprianka25@users.noreply.github.com>
2024-08-26 15:01:53 +0530

    Time: 18 ms (25.65%), Space: 55.3 MB (26.11%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
@@ -1,15 +1,32 @@
 class Solution {
     public List<Integer> findDuplicates(int[] nums) {
-        List<Integer> res = new ArrayList<>();
-        Map<Integer,Integer> map = new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            if(map.getOrDefault(nums[i],0)==0){
-                map.put(nums[i],1);
-            }else{
-                res.add(nums[i]);
+        //List<Integer> res = new ArrayList<>();
+        //Map<Integer,Integer> map = new HashMap<>();
+        //for(int i=0;i<nums.length;i++){
+            //if(map.getOrDefault(nums[i],0)==0){
+               // map.put(nums[i],1);
+            //}else{
+                //res.add(nums[i]);
+            //}
+       // }
+       // return res;
+        
+   // }
+//}
+        
+        
+        
+         List<Integer> duplicates = new ArrayList<>();
+        
+        Arrays.sort(nums); 
+        
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                duplicates.add(nums[i]); 
+                i++;
             }
         }
-        return res;
         
+        return duplicates; 
     }
 }
