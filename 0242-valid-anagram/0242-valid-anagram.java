f8e2694ecf379a4f658da8307142c3fbed9e0296
prianka dey <125360067+deyprianka25@users.noreply.github.com>
2024-09-02 11:20:56 +0530

    Time: 3 ms (89.05%), Space: 44.3 MB (54.93%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,19 +1,47 @@
 class Solution {
       public static boolean isAnagram(String s, String t) {
-           if (s.length() != t.length()) {
+                   if (s.length() != t.length()) {
             return false;
         }
-
-        // Convert both strings to character arrays
         char[] sArray = s.toCharArray();
         char[] tArray = t.toCharArray();
-
-        // Sort both character arrays
         Arrays.sort(sArray);
         Arrays.sort(tArray);
-
-        // Compare the sorted arrays
         return Arrays.equals(sArray, tArray);
     }
 }
-        
+      
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+         
