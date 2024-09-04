449bd779141130716b1e417c2d24e4f07b995138
prianka dey <125360067+deyprianka25@users.noreply.github.com>
2024-09-04 13:16:29 +0530

    Time: 33 ms (41.57%), Space: 52.2 MB (34.17%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,22 +1,62 @@
 class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        Set<List<Integer>> res = new HashSet<>();
-        if(nums.length==0)  return new ArrayList<>(res);
-        Arrays.sort(nums);
-        for(int i=0;i<nums.length-2;i++){
-            int j = i+1;
-            int k = nums.length-1;
-            while(j<k){
-                int sum = nums[j]+nums[k];
-                if(sum == -nums[i]){
-                    res.add(Arrays.asList(nums[i],nums[j],nums[k]));
-                    j++; k--;
+    public List<List<Integer>> threeSum(int[] arr) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(arr);
+        int n = arr.length;
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && arr[i] == arr[i - 1]) continue;
+
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+                int sum = arr[i] + arr[left] + arr[right];
+
+                if (sum == 0) {
+                    ArrayList<Integer> temp = new ArrayList<>();
+                    temp.add(arr[i]);
+                    temp.add(arr[left]);
+                    temp.add(arr[right]);
+                    ans.add(temp);
+                    while (left < right && arr[left] == arr[left + 1]) left++;
+                    while (left < right && arr[right] == arr[right - 1]) right--;
+
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
                 }
-                else if (sum>-nums[i]) k--;
-                else if (sum<-nums[i]) j++;
             }
         }
-        return new ArrayList<>(res);
-        
+        if (ans.isEmpty()) {
+            return new ArrayList<>();
+        }
+
+        return ans;
     }
 }
+
+        
+    
+        //Set<List<Integer>> res = new HashSet<>();
+        //if(nums.length==0)  return new ArrayList<>(res);
+        //Arrays.sort(nums);
+        //for(int i=0;i<nums.length-2;i++){
+            //int j = i+1;
+            //int k = nums.length-1;
+            //while(j<k){
+                //int sum = nums[j]+nums[k];
+                //if(sum == -nums[i]){
+                    //res.add(Arrays.asList(nums[i],nums[j],nums[k]));
+                    //j++; k--;
+                //}
+                //else if (sum>-nums[i]) k--;
+                ////else if (sum<-nums[i]) j++;
+            //}
+        //}
+        //return new ArrayList<>(res);
+        
+    //}
+//}
