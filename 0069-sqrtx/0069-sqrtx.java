71c62a9cde8374efb56befe1c062eec339065a96
prianka dey <125360067+deyprianka25@users.noreply.github.com>
2024-09-08 16:46:34 +0530

    Time: 1 ms (85.71%), Space: 41.2 MB (14.02%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,20 +1,23 @@
 class Solution {
+    private static  int square(int x,int s, int e){
+        if(s>e){
+            return e;
+        }
+        int mid = s+(e-s)/2;
+        if(mid==x/mid){
+            return mid;
+            
+        }
+        else if(mid<x/mid){
+            return square(x,mid+1,e);
+            
+        }
+        else{
+            return square(x,s,mid-1);
+        }
+    }
     public int mySqrt(int x) {
        if (x == 0) return 0;
-        int start = 1, end = x, mid, ans = 0;
-        
-        while (start <= end) {
-            mid = start + (end - start) / 2;
-            if (mid == x / mid) {
-                return mid;
-            } else if (mid < x / mid) {
-                ans = mid; 
-                start = mid + 1;
-            } else {
-                end = mid - 1;
-            }
-        }
-        
-        return ans;
+        return square(x,1,x);
     }
 }
