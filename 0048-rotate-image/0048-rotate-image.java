1e817573ebd8c5d61d6d416b87b70f534ef17027
prianka dey <125360067+deyprianka25@users.noreply.github.com>
2024-10-02 13:35:45 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (9.42%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,26 +1,29 @@
 class Solution {
     public void rotate(int[][] matrix) {
-         int n = matrix.length;
-
-        // Transpose the matrix
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
+        Swaprows(matrix);
+        Transpose(matrix);
+    }
+    public static void Swaprows(int[][] matrix){
+        int n = matrix.length;
+        int start =0;
+        int end = n-1;
+        while(start<end){
+            int []temp = matrix[start];
+            matrix[start] = matrix[end];
+            matrix[end] = temp;
+            start++;
+            end--;
+        }
+    }
+    public static void Transpose(int[][]matrix){
+        int n = matrix.length;
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
             }
         }
-
-        // Reverse each row
-        for (int i = 0; i < n; i++) {
-            int left = 0, right = n - 1;
-            while (left < right) {
-                int temp = matrix[i][left];
-                matrix[i][left] = matrix[i][right];
-                matrix[i][right] = temp;
-                left++;
-                right--;
-            }
-        }
     }
 }
+    
