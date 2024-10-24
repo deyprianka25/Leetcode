c02d53f31f9163f6276718986f4374f856fedb22
prianka dey <125360067+deyprianka25@users.noreply.github.com>
2024-10-24 22:00:00 +0530

    Time: 1 ms (92.90%), Space: 41.9 MB (6.41%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.java
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.java
@@ -1,12 +1,29 @@
 class Solution {
     public int minimumOperations(int[] nums) {
-      //APPROACH:HASHSET  
-        HashSet<Integer> unique = new HashSet<>();
-        for(int num:nums){
-            if(num>0){
-                unique.add(num);
+        //APPROACH:PRIORITY QUEUE
+          PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for (int num : nums) {
+            if (num > 0) {
+                minHeap.add(num);
             }
         }
-       return unique.size(); 
-    }
+        int operations = 0;
+        while (!minHeap.isEmpty()) {
+            int smallest = minHeap.poll();
+            operations++;
+            while (!minHeap.isEmpty() && minHeap.peek() == smallest) {
+                minHeap.poll();
+            }
+        }
+        
+        return operations;
+      //APPROACH:HASHSET  
+       // HashSet<Integer> unique = new HashSet<>();
+       // for(int num:nums){
+           // if(num>0){
+               // unique.add(num);
+           // }
+        //}
+      // return unique.size(); 
+}
 }
