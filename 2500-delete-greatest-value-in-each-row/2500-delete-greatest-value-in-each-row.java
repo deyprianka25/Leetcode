60a48990379114adea1b7cd50b86a49ae3bd4a7b
prianka dey <125360067+deyprianka25@users.noreply.github.com>
2024-10-25 07:44:00 +0530

    Time: 10 ms (15.36%), Space: 44.5 MB (37.06%) - LeetHub

--- 2500-delete-greatest-value-in-each-row/2500-delete-greatest-value-in-each-row.java
+++ 2500-delete-greatest-value-in-each-row/2500-delete-greatest-value-in-each-row.java
@@ -1,19 +1,42 @@
 class Solution {
     public int deleteGreatestValue(int[][] grid) {
-        //APPROACH:SIMPLE
+        //APPROACH:PRIORITY QUEUE
         int m = grid.length;
         int n = grid[0].length;
         int answer = 0;
-        for(int i =0;i<m;i++){
-            Arrays.sort(grid[i]);
+        PriorityQueue<Integer>[] rowQueues = new PriorityQueue[m];
+        for (int i = 0; i < m; i++) {
+            rowQueues[i] = new PriorityQueue<>(Collections.reverseOrder());
+            for (int j = 0; j < n; j++) {
+                rowQueues[i].offer(grid[i][j]); 
+            }
         }
-        for(int col= n-1;col>=0;col--){
-            int maxval =0;
+        for (int col = 0; col < n; col++) {
+            int maxVal = 0;
             for (int row = 0; row < m; row++) {
-                maxval = Math.max(maxval, grid[row][col]);
+                int removedValue = rowQueues[row].poll();  
+                maxVal = Math.max(maxVal, removedValue);   
+            }
+            answer += maxVal;
         }
-            answer+=maxval;    
-    }
+
         return answer;
+
+        
+        //APPROACH:SIMPLE
+        //int m = grid.length;
+        //int n = grid[0].length;
+        //int answer = 0;
+        //for(int i =0;i<m;i++){
+            //Arrays.sort(grid[i]);
+       // }
+       // for(int col= n-1;col>=0;col--){
+            //int maxval =0;
+            //for (int row = 0; row < m; row++) {
+                //maxval = Math.max(maxval, grid[row][col]);
+       // }
+           // answer+=maxval;    
+    //}
+        //return answer;
 }
 }
