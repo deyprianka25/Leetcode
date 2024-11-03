8675b00ee52db5435a291146960ec10ef8b20046
prianka dey <125360067+deyprianka25@users.noreply.github.com>
2024-11-03 20:49:06 +0530

    Time: 1 ms (100.00%), Space: 41.8 MB (100.00%) - LeetHub

---
+++ 3340-check-balanced-string/3340-check-balanced-string.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isBalanced(String num) {
+        long odd =0;
+        long even = 0;
+        for(int i=0;i<num.length();i++){
+            if(i%2==0){
+                even += num.charAt(i)-'0';
+            }else{
+                odd += num.charAt(i)-'0';
+            }
+        }
+        return odd==even;
+        
+        
+    }
+}
