ae2e5b941c962ac5c3e1526fba0b044c9c119f66
prianka dey <125360067+deyprianka25@users.noreply.github.com>
2024-11-24 22:23:53 +0530

    Time: 1 ms, Memory: 40.2 MB - LeetHub

---
+++ happy-number/happy-number.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean isHappy(int n) {
+    int slow = squaredSum(n);
+    int fast = squaredSum(squaredSum(n));
+
+    while (slow != fast) {
+      slow = squaredSum(slow);
+      fast = squaredSum(squaredSum(fast));
+    }
+
+    return slow == 1;
+  }
+
+  private int squaredSum(int n) {
+    int sum = 0;
+    while (n > 0) {
+      sum += Math.pow(n % 10, 2);
+      n /= 10;
+    }
+    return sum;
+  }
+}
+
+       // HashSet<Integer> hs = new HashSet<>();
+        //int sum = 0;
+        //while(sum!=1 && !hs.contains(sum)){
+            //sum = 0;
+           //// while(n!=0){
+                //sum+=Math.pow(n%10,2);
+                //n/=10;
+            //}
+            //n=sum;
+        //}
+        //return sum==1;
+        
+        
+    //}
+//}
