dfe6c36f873c0a7a980462959a60c0df3ec9ee1b
prianka dey <125360067+deyprianka25@users.noreply.github.com>
2024-11-24 22:37:51 +0530

    Time: 1 ms, Memory: 41 MB - LeetHub

---
+++ sqrtx/sqrtx.java
@@ -0,0 +1,24 @@
+class Solution {
+    //RECURSION.
+    private static  int square(int x,int s, int e){
+        if(s>e){
+            return e;
+        }
+        int mid = s+(e-s)/2;
+        if(mid==x/mid){
+            return mid;
+            
+        }
+        else if(mid<x/mid){
+            return square(x,mid+1,e);
+            
+        }
+        else{
+            return square(x,s,mid-1);
+        }
+    }
+    public int mySqrt(int x) {
+       if (x == 0) return 0;
+        return square(x,1,x);
+    }
+}
